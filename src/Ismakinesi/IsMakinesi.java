2b19cfd6ea21a07a97384edf71f18249bd8caf3d
Anıl Kaynar <aanilkay@gmail.com>
2018-05-13 23:05:44 +0300

    toString metodu implemente edildi.

--- src/Ismakinesi/IsMakinesi.java
+++ src/Ismakinesi/IsMakinesi.java
@@ -13,11 +13,13 @@ public class IsMakinesi {
     Scanner scanner;
     PrintWriter writer;
     MakineTuru makineTuru;
+    int birimhiz;
     String statu;
     String makineId;
 
     public IsMakinesi(String name, MakineTuru makineTuru) {
         this.name = name;
+        int k = birimhiz;
         this.makineTuru = makineTuru;
     }
 
@@ -29,5 +31,10 @@ public class IsMakinesi {
 
     public void doWork(){
 
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%s %s %d %d", name, makineTuru.name(), birimhiz, makineId);
     }
 }
